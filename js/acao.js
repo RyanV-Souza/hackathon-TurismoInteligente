eb1f32b86c3dbc8c15829a04e40fa945b5bd2574
RyanV-Souza <ryanvsouza99@gmail.com>
2019-09-22 12:48:17 -0300

    Adicionando div para a inserção de texto, organizando os "murais" e reajustando o HREF com JavaScript

--- js/acao.js
+++ js/acao.js
@@ -12,3 +12,18 @@ $(document).on("click", ".nav3", function(){
 
   $(location).attr('href', 'eventos.html');
 });
+
+$(document).on("click", ".conteudo3", function(){
+
+  window.open('https://www.google.com/search?q=Restaurante+em+Mongagu%C3%A1&oq=Restaurante+em+Mongagu%C3%A1&aqs=chrome..69i57j0l5.5351j0j9&sourceid=chrome&ie=UTF-8', '_blank');
+})
+
+$(document).on("click", ".conteudo1", function(){
+
+  window.open ('https://www.google.com/search?ei=G7uDXYfCLqm65OUP0uWx0A8&q=Lazer+em+Mongagu%C3%A1&oq=Lazer+em+Mongagu%C3%A1&gs_l=psy-ab.3...37119.38799..38975...2.0..0.90.669.8......0....1..gws-wiz.......0i7i30j0i7i5i30.16J9PxUrma8&ved=0ahUKEwjHu7HutN3kAhUpHbkGHdJyDPoQ4dUDCAs&uact=5', '_blank');
+})
+
+$(document).on("click", ".conteudo2", function(){
+
+  window.open('https://www.google.com/search?ei=Q7uDXbz_HYO_5OUPi_ivmAk&q=Hotelaria+em+Mongagu%C3%A1&oq=Hotelaria+em+Mongagu%C3%A1&gs_l=psy-ab.3...47264.48249..48335...0.0..0.175.905.6j3......0....1..gws-wiz.......38j0i7i30j0j0i30j0i13j0i13i30j0i8i13i30j0i13i5i30.pFpQ06wbwKM&ved=0ahUKEwj8rKqBtd3kAhWDH7kGHQv8C5MQ4dUDCAs&uact=5', '_blank');
+})
